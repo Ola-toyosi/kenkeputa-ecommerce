2f9b3c5bf6c327329e73a67f5066aa99d09a7489
t0yosi <177050929+t0yosi@users.noreply.github.com>
2025-09-30 12:24:47 +0100

    Cors backend

--- server/config/settings.py
+++ server/config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "users",
     "products",
@@ -55,6 +56,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -83,6 +85,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8081",
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Database
